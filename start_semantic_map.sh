b92cbe82e5ffc69c9568c06f37af92d46e6f87f5
SCzhJ <100019407+SCzhJ@users.noreply.github.com>
2025-06-27 21:03:29 +0800

    Update start_semantic_map.sh

--- start_semantic_map.sh
+++ start_semantic_map.sh
@@ -7,7 +7,7 @@ tmux new-session -d -s sem_map_yolo_lseg "ros2 run sem_map sem_map_service_yolo_
 
 sleep 10
 
-tmux new-session -d -s image_socket_recv "ros2 run sem_map image_socket_recv" 
+tmux new-session -d -s image_transform_client.py "ros2 run sem_map image_transform_client.py" 
 tmux new-session -d -s query_socket_handler "ros2 run sem_map query_socket_handler" 
 
 echo "Sessions Started:"
